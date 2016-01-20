0dd7c0ed1e5561aeca424c17a234369b51123c8e
ball <ballzacup0@gmail.com>
2016-01-19 19:46:00 -0800

    Bind Widget find

--- app/src/main/java/th/ac/krirk/ball/ball/MainActivity.java
+++ app/src/main/java/th/ac/krirk/ball/ball/MainActivity.java
@@ -2,12 +2,34 @@ package th.ac.krirk.ball.ball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+    private ImageView paperImageView, rockImageView,
+            scissorImageView, playImageView , androidImageView;
+    private TextView showTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+        paperImageView = (ImageView) findViewById(R.id.imvpaper);
+        rockImageView = (ImageView) findViewById(R.id.imvrock);
+        scissorImageView = (ImageView) findViewById(R.id.imvscissors);
+        playImageView = (ImageView) findViewById(R.id.imvplayer);
+        androidImageView = (ImageView) findViewById(R.id.imvAndroid);
     }
-}
+
+}    // Main Class นี้คือคลาสหลัก
